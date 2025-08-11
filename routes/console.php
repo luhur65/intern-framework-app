ac9fcfaf239b3fd35f3bc4d84c6482af518ea234
Dharma Situmorang <dharmabakti1202@gmail.com>
2025-08-11 13:07:42 +0700

    add: menambahkan perintah cli custom di artisan

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('creator', function() {
+    $this->comment("Created By: @dharma_stmrng");
+})->purpose('Display a creator info');
